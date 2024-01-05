b414e75d5ac55edc70b8d4339474c3d2ec2ef6b7
Can Doruk <149999928+zSpawnKara@users.noreply.github.com>
2024-01-05 22:45:44 +0300

    Update idstick.m

--- Module-2.3/idstick.m
+++ Module-2.3/idstick.m
@@ -12,9 +12,14 @@ classdef idstick
     end
 % Sınıfınızın metodlarını, sınıf tanımlamasındaki bir methods bloğu içinde standart fonksiyon bildirimi sözdizimini kullanarak tanımlayabilirsiniz. Bir metodun ilk girişi her zaman nesne olacaktır.
     methods
-        function signal(id,ok)
+         function signal(id,ok)
             if ok
-                disp("Beep")
+            % Nesne bir giriş olarak geçirildiği için, metod, düzenli nokta indeksleme kullanarak nesnenin tüm özelliklerine erişebilir.
+                if (id.Status == "Ready") || (id.Status == "Done") 
+                    disp("Beep beep")
+                else
+                    disp("Beep")
+                end
             else
                 disp("Buzz")
             end
