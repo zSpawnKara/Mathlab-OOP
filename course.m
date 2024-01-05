491438dd4d8d8658580d29eaadefd6e9b8ca1101
Can Doruk <149999928+zSpawnKara@users.noreply.github.com>
2024-01-05 22:23:33 +0300

    Update course.m

--- course.m
+++ course.m
@@ -1,4 +1,9 @@
 % Yeni bir sınıf oluşturmak için classname adında bir sınıf tanımlama bloğu içeren bir (düz metin) kod dosyası oluşturun. Bu dosyanın adı classname.m olmalıdır.
 classdef course
-    
+    % Sınıfınızın özelliklerini, sınıf tanımlamasındaki bir properties bloğu içinde özellik isimlerini listeleyerek tanımlayabilirsiniz:
+    properties
+        Name
+        Level
+        Waypoints
+    end 
 end
