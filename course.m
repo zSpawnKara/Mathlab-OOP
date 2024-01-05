31234e7d724a9e6c9e0829a02d03aa8b4b7736d9
Can Doruk <149999928+zSpawnKara@users.noreply.github.com>
2024-01-05 22:19:45 +0300

    Update course.m

--- course.m
+++ course.m
@@ -1,1 +1,4 @@
-
+% Yeni bir sınıf oluşturmak için classname adında bir sınıf tanımlama bloğu içeren bir (düz metin) kod dosyası oluşturun. Bu dosyanın adı classname.m olmalıdır.
+classdef course
+    
+end
