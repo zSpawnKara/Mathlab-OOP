ffa0b32c52a46b58cb60c5d8d6e7336551bb4ce6
Can Doruk <149999928+zSpawnKara@users.noreply.github.com>
2024-01-05 22:33:34 +0300

    Create idstick.m

---
+++ Module-2.2/idstick.m
@@ -0,0 +1,14 @@
+% Bir Classı Diğer Class ile kullanmak
+
+% Sınıfınızın özelliklerini, sınıf tanımlamasındaki bir properties bloğu içinde özellik isimlerini listeleyerek tanımlayabilirsiniz
+
+classdef idstick
+    properties
+        SerialNumber
+        Status
+        Participant
+        Course
+        Timestamps
+    end
+    
+end
